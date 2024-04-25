d1e87c1eee98173c0399d2aaece1fd79ad6489a8
Nesrux <joaomarcosdevs@gmail.com>
2024-04-25 14:07:24 -0300

    test: adiciona teste categoria

--- domain/src/test/java/com/nesrux/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/nesrux/admin/catalogo/domain/category/CategoryTest.java
@@ -1,4 +1,13 @@
 package com.nesrux.admin.catalogo.domain.category;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CategoryTest {
+    @Test
+    public void test(){
+        var category = new Category();
+
+        Assertions.assertNotNull(category);
+    }
 }
