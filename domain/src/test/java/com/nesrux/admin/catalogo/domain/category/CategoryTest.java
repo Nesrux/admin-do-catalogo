125d0f9c915df82d47deff4e33e382bc6032cb8d
Nesrux <joaomarcosdevs@gmail.com>
2024-04-25 14:34:16 -0300

    test: adiciona teste categoria

--- domain/src/test/java/com/nesrux/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/nesrux/admin/catalogo/domain/category/CategoryTest.java
@@ -1,13 +1,32 @@
 package com.nesrux.admin.catalogo.domain.category;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CategoryTest {
     @Test
-    public void test(){
-        var category = new Category();
+    @DisplayName("instancia nova categoria")
+    public void givenAvalidParams_whenCallNewCategory_theninstatiateACategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
 
-        Assertions.assertNotNull(category);
+
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+
+        assertNotNull(actualCategory);
+        assertNotNull(actualCategory.getId());
+        assertEquals(expectedName, actualCategory.getName());
+        assertEquals(expectedDescription, actualCategory.getDescription());
+        assertEquals(expectedIsActive, actualCategory.isActive());
+        assertNotNull(actualCategory.getCreatedAt());
+        assertNotNull(actualCategory.getUpdatedAt());
+        assertNull(actualCategory.getDeletedAt());
+
+
     }
 }
