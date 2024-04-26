3fa52d2102045e6352b5bbca02e4c325fe332101
Nesrux <joaomarcosdevs@gmail.com>
2024-04-26 17:13:18 -0300

    test: adiciona novo teste a Entity categoria

--- domain/src/test/java/com/nesrux/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/nesrux/admin/catalogo/domain/category/CategoryTest.java
@@ -1,5 +1,7 @@
 package com.nesrux.admin.catalogo.domain.category;
 
+import com.nesrux.admin.catalogo.domain.exceptions.DomainException;
+import com.nesrux.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -29,4 +31,25 @@ public class CategoryTest {
 
 
     }
+
+    @Test
+    @DisplayName("Falha ao validar nova categoria")
+    public void givenAnIvalidName_whenCallNewCategoryAndValidate_ThenShouldReciveError() {
+        final String expectedName = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var actualException = assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+
+    }
+
+
 }
