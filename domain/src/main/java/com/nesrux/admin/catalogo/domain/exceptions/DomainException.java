eb9e5d7f68bd2a1181cb771e1b34e6c0550d8988
Nesrux <joaomarcosdevs@gmail.com>
2024-04-26 19:07:23 -0300

    refactor: refatora as exceptions

--- domain/src/main/java/com/nesrux/admin/catalogo/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/nesrux/admin/catalogo/domain/exceptions/DomainException.java
@@ -4,16 +4,20 @@ import com.nesrux.admin.catalogo.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStacktraceException {
     private final List<Error> errors;
 
-    public DomainException(final List<Error> errors) {
-        super("", null, true, false); // <- ajuda na performance!
+    public DomainException(final String aMessage, final List<Error> errors) {
+        super(aMessage); // <- ajuda na performance!
         this.errors = errors;
     }
 
     public static DomainException with(final List<Error> anErrors) {
-        return new DomainException(anErrors);
+        return new DomainException("", anErrors);
+    }
+
+    public static DomainException with(final Error anError) {
+        return new DomainException(anError.message(), List.of(anError));
     }
 
     public List<Error> getErrors() {
