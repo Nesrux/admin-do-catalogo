a35fe79ef71b58f863004ab00bf5e7194e649093
Nesrux <joaomarcosdevs@gmail.com>
2024-04-28 12:20:57 -0300

    feat:  cria usecase de category

---
+++ application/src/main/java/com/nesrux/admin/catalogo/application/category/create/CreateCategoryOutput.java
@@ -0,0 +1,11 @@
+package com.nesrux.admin.catalogo.application.category.create;
+
+import com.nesrux.admin.catalogo.domain.category.Category;
+import com.nesrux.admin.catalogo.domain.category.CategoryId;
+
+public record CreateCategoryOutput(CategoryId id) {
+
+    public static CreateCategoryOutput from(final Category aCategory) {
+        return new CreateCategoryOutput(aCategory.getId());
+    }
+}
---
+++ application/src/main/java/com/nesrux/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,7 @@
+package com.nesrux.admin.catalogo.application.category.create;
+
+import com.nesrux.admin.catalogo.application.UseCase;
+
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+
+}
