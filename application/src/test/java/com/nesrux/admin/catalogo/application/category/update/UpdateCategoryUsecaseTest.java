10152c679f594cc57716f6507b9be6f43bced75d
Nesrux <joaomarcosdevs@gmail.com>
2024-04-29 16:34:35 -0300

    test:  testa usecase update de categoria

---
+++ application/src/test/java/com/nesrux/admin/catalogo/application/category/update/UpdateCategoryUsecaseTest.java
@@ -0,0 +1,74 @@
+package com.nesrux.admin.catalogo.application.category.update;
+
+import com.nesrux.admin.catalogo.domain.category.Category;
+import com.nesrux.admin.catalogo.domain.category.CategoryGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateCategoryUsecaseTest {
+    @InjectMocks
+    private DefaultUpdateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+
+    @Test
+    public void givenAvalidCommand_whenCallsUpdateCategory_shouldReturnCategoryId() {
+        final var aCategory = Category.newCategory("Filne", null, false);
+        final var expectedName = "filme";
+        final var expectedDescription = "A categoria mais acessada";
+        final var expectedIsActive = true;
+
+
+        final var expectedId = aCategory.getId();
+
+        final var aCommand = UpdateCategoryCommand.with(
+                aCategory.getId().getValue(),
+                expectedName, expectedDescription,
+                expectedIsActive);
+
+        when(categoryGateway
+                .findById(eq(expectedId)))
+                .thenReturn(Optional.of(aCategory));
+
+        when(categoryGateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand).get();
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        verify(categoryGateway, times(1))
+                .findById(expectedId);
+
+
+        verify(categoryGateway, times(1))
+                .update(argThat(aUpdatedCategory ->
+                        Objects.equals(expectedName, aUpdatedCategory.getName())
+                                && Objects.equals(expectedDescription, aUpdatedCategory.getDescription())
+                                && Objects.equals(expectedIsActive, aUpdatedCategory.isActive())
+                                && Objects.equals(expectedId, aUpdatedCategory.getId())
+                                && Objects.equals(aCategory.getCreatedAt(), aUpdatedCategory.getCreatedAt())
+                                && Objects.isNull(aUpdatedCategory.getDeletedAt())
+                                && aUpdatedCategory.getUpdatedAt().isAfter(aCategory.getUpdatedAt())
+                ));
+//                .update(argThat(aUpdatedCategory -> Objects.equals(expectedName, aUpdatedCategory.getName()));
+
+    }
+}
+
+
