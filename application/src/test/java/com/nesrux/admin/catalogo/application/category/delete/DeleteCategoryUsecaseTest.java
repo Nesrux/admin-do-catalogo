36ff4fc6283b0d71ab52479f672f2bb4e5870459
Nesrux <joaomarcosdevs@gmail.com>
2024-05-02 17:39:16 -0300

    test: adiciona teste de delete usecase de categoria

---
+++ application/src/test/java/com/nesrux/admin/catalogo/application/category/delete/DeleteCategoryUsecaseTest.java
@@ -0,0 +1,71 @@
+package com.nesrux.admin.catalogo.application.category.delete;
+
+import com.nesrux.admin.catalogo.domain.category.Category;
+import com.nesrux.admin.catalogo.domain.category.CategoryGateway;
+import com.nesrux.admin.catalogo.domain.category.CategoryId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteCategoryUsecaseTest {
+    @InjectMocks
+    private DefaultDeleteCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp() {
+        reset(categoryGateway);
+    }
+
+    /*
+     * 1) onde eu consigo deletar com id valido
+     * 2) não consigo deletar pois o id esta invalido
+     * 3) erro no gateway*/
+    @Test
+    public void givenAValidId_whenCallsDeleteCategory_shouldBeOk() {
+        final var aCategory = Category.newCategory("Filmes", "só os filés", true);
+        final var expectedId = aCategory.getId();
+
+        doNothing()
+                .when(categoryGateway).deleteById(eq(expectedId));
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+    }
+
+    @Test
+    public void givenAInvalidId_whenCallsDeleteCategory_shouldBeOk() {
+        final var expectedId = CategoryId.from("123");
+
+        doNothing()
+                .when(categoryGateway).deleteById(eq(expectedId));
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+
+    }
+
+    @Test
+    public void givenAValidId_whenGatewayThrowsException_shouldReturnException() {
+        final var aCategory = Category.newCategory("Filmes", "só os filés", true);
+        final var expectedId = aCategory.getId();
+
+        doThrow(new IllegalStateException("Gateway error"))
+                .when(categoryGateway).deleteById(eq(expectedId));
+        Assertions.assertThrows( IllegalStateException.class, () -> useCase.execute(expectedId.getValue()));
+
+        verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+
+    }
+
+}
