a114896b587dafaba3155a75716b9cbb426b2945
Nesrux <joaomarcosdevs@gmail.com>
2024-05-04 14:04:48 -0300

    feat: configuraçã inicial do Spring

---
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.nesrux.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.nesrux.admin.catalogo")
+public class WebServerConfig {
+}
