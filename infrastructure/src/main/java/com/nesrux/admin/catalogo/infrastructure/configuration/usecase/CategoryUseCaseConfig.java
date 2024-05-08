b0058d78c1149934c9b4c8db6e33139237062bed
Nesrux <joaomarcosdevs@gmail.com>
2024-05-08 15:44:47 -0300

    feat: criando os beans de usecase de categoria

---
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/configuration/usecase/CategoryUseCaseConfig.java
@@ -0,0 +1,50 @@
+package com.nesrux.admin.catalogo.infrastructure.configuration.usecase;
+
+import com.nesrux.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.nesrux.admin.catalogo.application.category.create.DefaultCreateCategoryUseCase;
+import com.nesrux.admin.catalogo.application.category.delete.DefaultDeleteCategoryUseCase;
+import com.nesrux.admin.catalogo.application.category.delete.DeleteCategoryUseCase;
+import com.nesrux.admin.catalogo.application.category.retrive.get.DefaultGetCategoryByIdUseCase;
+import com.nesrux.admin.catalogo.application.category.retrive.get.GetCategoryByIdUseCase;
+import com.nesrux.admin.catalogo.application.category.retrive.list.DefaultListCategoryUseCase;
+import com.nesrux.admin.catalogo.application.category.retrive.list.ListCategoriesUSeCase;
+import com.nesrux.admin.catalogo.application.category.update.DefaultUpdateCategoryUseCase;
+import com.nesrux.admin.catalogo.application.category.update.UpdateCategoryUseCase;
+import com.nesrux.admin.catalogo.domain.category.CategoryGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CategoryUseCaseConfig {
+
+    private final CategoryGateway gateway;
+
+    public CategoryUseCaseConfig(CategoryGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    @Bean
+    public CreateCategoryUseCase createCategoryUseCase() {
+        return new DefaultCreateCategoryUseCase(gateway);
+    }
+
+    @Bean
+    public UpdateCategoryUseCase updateCategoryUseCase() {
+        return new DefaultUpdateCategoryUseCase(gateway);
+    }
+
+    @Bean
+    public DeleteCategoryUseCase deleteCategoryUseCase() {
+        return new DefaultDeleteCategoryUseCase(gateway);
+    }
+
+    @Bean
+    public GetCategoryByIdUseCase getCategoryByIdUseCase() {
+        return new DefaultGetCategoryByIdUseCase(gateway);
+    }
+
+    @Bean
+    public ListCategoriesUSeCase listCategoriesUSeCase() {
+        return new DefaultListCategoryUseCase(gateway);
+    }
+}
