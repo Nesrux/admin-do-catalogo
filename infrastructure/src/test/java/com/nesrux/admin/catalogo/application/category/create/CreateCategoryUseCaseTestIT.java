38fec8ba94a19e6b4f9d35bd0aeeafd4abdb9dfb
Nesrux <joaomarcosdevs@gmail.com>
2024-05-09 17:14:40 -0300

    test: adiciona teste de integração usecase create category

---
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/application/category/create/CreateCategoryUseCaseTestIT.java
@@ -0,0 +1,44 @@
+package com.nesrux.admin.catalogo.application.category.create;
+
+import com.nesrux.admin.catalogo.IntegrationTest;
+import com.nesrux.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@IntegrationTest
+public class CreateCategoryUseCaseTestIT {
+
+    @Autowired
+    private CreateCategoryUseCase useCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void givenAvalidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
+        final var expectedName = "filmes";
+        final var expectedDescription = "A categoria mais assitida";
+        final var expectedIsActive = true;
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualOutput = useCase.execute(aCommand).get();
+        //final var actualOutput = useCase.execute(aCommand).get();
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        final var actualCategory =
+                categoryRepository.findById(actualOutput.id().getValue()).get();
+
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+
+
+    }
+}
