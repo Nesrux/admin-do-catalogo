9f062e4c99633855bf354ef0cb4e6cd15ae252b4
Nesrux <joaomarcosdevs@gmail.com>
2024-05-12 13:17:49 -0300

    refactor: troca o nome da classe

--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -1,12 +1,21 @@
 package com.nesrux.admin.catalogo.infrastructure.api.controllers;
 
+import com.nesrux.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import com.nesrux.admin.catalogo.domain.pagination.Pagination;
-import com.nesrux.admin.catalogo.infrastructure.api.CategoryApi;
+import com.nesrux.admin.catalogo.infrastructure.api.CategoryAPI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
-public class CategoryController implements CategoryApi {
+public class CategoryController implements CategoryAPI {
+    private final CreateCategoryUseCase createUseCase;
+
+    public CategoryController(CreateCategoryUseCase createUseCase) {
+        Objects.requireNonNull(this.createUseCase = createUseCase);
+    }
+
     @Override
     public ResponseEntity<?> createCategory() {
         return null;
