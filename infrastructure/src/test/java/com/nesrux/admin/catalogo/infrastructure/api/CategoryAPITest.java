1825926382ec1a8807424c733f6823a222195e84
Nesrux <joaomarcosdevs@gmail.com>
2024-05-13 14:09:56 -0300

    test: adiciona teste de controllers de Category

--- infrastructure/src/test/java/com/nesrux/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -1,14 +1,30 @@
 package com.nesrux.admin.catalogo.infrastructure.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nesrux.admin.catalogo.ControllerTest;
+import com.nesrux.admin.catalogo.application.category.create.CreateCategoryOutput;
 import com.nesrux.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.nesrux.admin.catalogo.domain.category.CategoryId;
+import com.nesrux.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Objects;
+
+import static io.vavr.API.Right;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ControllerTest(controllers = CategoryAPI.class)
 public class CategoryAPITest {
+    @Autowired
+    private ObjectMapper mapper;
 
     @Autowired
     private MockMvc mvc;
@@ -17,7 +33,35 @@ public class CategoryAPITest {
     private CreateCategoryUseCase createCategoryUseCase;
 
     @Test
-    public void test() {
+    public void givenAValidCommand_WhenCallsCreateCAtegory_ShouldReturnCategoryID() throws Exception {
+        final var expectedName = "filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aInput =
+                new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenReturn(Right(CreateCategoryOutput
+                        .from(CategoryId.from("123"))));
+
+        // Classe importante
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpectAll(
+                        status().isCreated(),
+                        header().string("Location", "/categories/123")
+                );
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
 
     }
 }
