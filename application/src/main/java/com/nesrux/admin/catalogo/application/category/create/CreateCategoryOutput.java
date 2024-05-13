3df3081eb9462552ed033cbf4b02a5c8e480ec15
Nesrux <joaomarcosdevs@gmail.com>
2024-05-13 14:38:18 -0300

    feat: implementa o create de CategoryController

--- application/src/main/java/com/nesrux/admin/catalogo/application/category/create/CreateCategoryOutput.java
+++ application/src/main/java/com/nesrux/admin/catalogo/application/category/create/CreateCategoryOutput.java
@@ -1,15 +1,14 @@
 package com.nesrux.admin.catalogo.application.category.create;
 
 import com.nesrux.admin.catalogo.domain.category.Category;
-import com.nesrux.admin.catalogo.domain.category.CategoryId;
 
-public record CreateCategoryOutput(CategoryId id) {
+public record CreateCategoryOutput(String id) {
 
     public static CreateCategoryOutput from(final Category aCategory) {
-        return new CreateCategoryOutput(aCategory.getId());
+        return new CreateCategoryOutput(aCategory.getId().getValue());
     }
 
-    public static CreateCategoryOutput from(final CategoryId anId) {
+    public static CreateCategoryOutput from(final String anId) {
         return new CreateCategoryOutput(anId);
     }
 
--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -1,13 +1,19 @@
 package com.nesrux.admin.catalogo.infrastructure.api.controllers;
 
+import com.nesrux.admin.catalogo.application.category.create.CreateCategoryCommand;
+import com.nesrux.admin.catalogo.application.category.create.CreateCategoryOutput;
 import com.nesrux.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import com.nesrux.admin.catalogo.domain.pagination.Pagination;
+import com.nesrux.admin.catalogo.domain.validation.handler.Notification;
 import com.nesrux.admin.catalogo.infrastructure.api.CategoryAPI;
+import com.nesrux.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.Objects;
+import java.util.function.Function;
 
 @RestController
 public class CategoryController implements CategoryAPI {
@@ -19,8 +25,19 @@ public class CategoryController implements CategoryAPI {
     }
 
     @Override
-    public ResponseEntity<?> createCategory() {
-        return null;
+    public ResponseEntity<?> createCategory(CreateCategoryApiInput input) {
+        final var aCommand = CreateCategoryCommand.with(
+                input.name(),
+                input.description(),
+                input.active() != null ? input.active() : true);
+
+        //Eu poderia devolver tudo isso em uma mesma função, mas ficaria menos legivel :(
+        final Function<Notification, ResponseEntity<?>> onError = ResponseEntity.unprocessableEntity()::body;
+        final Function<CreateCategoryOutput, ResponseEntity<?>> onSuccess = output -> ResponseEntity
+                .created(URI.create("/categories/" + output.id())).body(output);
+
+        return this.createUseCase.execute(aCommand)
+                .fold(onError, onSuccess);
     }
 
     @Override
