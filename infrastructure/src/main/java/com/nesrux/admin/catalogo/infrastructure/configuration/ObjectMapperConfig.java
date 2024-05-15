29a0f3ab33a51ae625bf1f1ad1c154ad214e74d8
Nesrux <joaomarcosdevs@gmail.com>
2024-05-15 19:31:10 -0300

    chore: configura o model mapper

--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/configuration/ObjectMapperConfig.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/configuration/ObjectMapperConfig.java
@@ -1,4 +1,14 @@
 package com.nesrux.admin.catalogo.infrastructure.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nesrux.admin.catalogo.infrastructure.configuration.json.Json;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectMapperConfig {
+    @Bean
+    public ObjectMapper mapper(){
+        return Json.mapper();
+    }
 }
