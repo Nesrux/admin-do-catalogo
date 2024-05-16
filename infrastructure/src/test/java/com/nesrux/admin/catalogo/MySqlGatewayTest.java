bd9534857e413b38b79357edc2373801030415c1
Nesrux <joaomarcosdevs@gmail.com>
2024-05-16 18:04:06 -0300

    refactor: altera o nome da classe

--- infrastructure/src/test/java/com/nesrux/admin/catalogo/MySqlGatewayTest.java
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/MySqlGatewayTest.java
@@ -17,7 +17,7 @@ import java.lang.annotation.*;
 @ComponentScan(includeFilters = {
         @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".[MySQLGateway]")
 })
-@ExtendWith(CleanUpExtension.class)
+@ExtendWith(MySQLCleanUpExtension.class)
 public @interface MySqlGatewayTest {
 
 }
