80e27e55f199d924d3fb358742bd4bf44dab1b8c
Nesrux <joaomarcosdevs@gmail.com>
2024-05-16 17:42:11 -0300

    test: adiciona teste de Unmarshall de CategoryResponse

--- infrastructure/src/test/java/com/nesrux/admin/catalogo/infrastructure/category/CategoryResponseTest.java
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/infrastructure/category/CategoryResponseTest.java
@@ -45,4 +45,47 @@ public class CategoryResponseTest {
 
     }
 
+    @Test
+    public void testUnMarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedUpdatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+        final var json = """
+                {
+                  "id": "%s",
+                  "name": "%s",
+                  "description": "%s",
+                  "is_active": %s,
+                  "created_at": "%s",
+                  "deleted_at": "%s",
+                  "updated_at": "%s"
+                }    
+                """.formatted(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedIsActive,
+                expectedCreatedAt.toString(),
+                expectedDeletedAt.toString(),
+                expectedUpdatedAt.toString()
+        );
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("id", expectedId)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive)
+                .hasFieldOrPropertyWithValue("createdAt", expectedCreatedAt)
+                .hasFieldOrPropertyWithValue("deletedAt", expectedDeletedAt)
+                .hasFieldOrPropertyWithValue("updatedAt", expectedUpdatedAt);
+
+    }
+
 }
