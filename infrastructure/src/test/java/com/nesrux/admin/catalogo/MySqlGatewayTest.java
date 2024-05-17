cf700474ecfb95a26e4e66897574be864ac69815
Nesrux <joaomarcosdevs@gmail.com>
2024-05-17 15:54:28 -0300

    refactor: altera o nome do arquivo para test-integration

--- infrastructure/src/test/java/com/nesrux/admin/catalogo/MySqlGatewayTest.java
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/MySqlGatewayTest.java
@@ -12,7 +12,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 
-@ActiveProfiles("test")
+@ActiveProfiles("test-integration")
 @DataJpaTest
 @ComponentScan(includeFilters = {
         @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".[MySQLGateway]")
