c9c6df511b97a5df4ea85e1953f0f80b065b345c
Nesrux <joaomarcosdevs@gmail.com>
2024-05-17 16:35:08 -0300

    feat: configura test e2e ao projeto

---
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/e2e/category/CategoryE2ETest.java
@@ -0,0 +1,34 @@
+package com.nesrux.admin.catalogo.e2e.category;
+
+import com.nesrux.admin.catalogo.E2ETest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@E2ETest
+public class CategoryE2ETest {
+
+    @Container
+    private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:latest")
+            .withPassword("123456")
+            .withUsername("root")
+            .withDatabaseName("adm_videos");
+
+    @DynamicPropertySource
+    public static void setDataSourceProperties(final DynamicPropertyRegistry registry) {
+        final var mappedPort = MYSQL_CONTAINER.getMappedPort(3306);
+        System.out.printf("Container is running on port %s\n", mappedPort);
+        registry.add("mysql_port", () -> mappedPort);
+
+    }
+
+    @Test
+    public void testWorks() {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+    }
+}
