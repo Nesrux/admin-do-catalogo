36d9e10997416b55a9b8ef70872c0279205b6525
Nesrux <joaomarcosdevs@gmail.com>
2024-05-17 17:03:30 -0300

    feat: adiciona teste e2e ao create category

--- infrastructure/src/test/java/com/nesrux/admin/catalogo/e2e/category/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/e2e/category/CategoryE2ETest.java
@@ -1,17 +1,34 @@
 package com.nesrux.admin.catalogo.e2e.category;
 
 import com.nesrux.admin.catalogo.E2ETest;
+import com.nesrux.admin.catalogo.domain.category.CategoryId;
+import com.nesrux.admin.catalogo.infrastructure.category.models.CategoryResponse;
+import com.nesrux.admin.catalogo.infrastructure.category.models.CreateCategoryRequest;
+import com.nesrux.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import com.nesrux.admin.catalogo.infrastructure.configuration.json.Json;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @Testcontainers
 @E2ETest
 public class CategoryE2ETest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Container
     private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:latest")
@@ -28,7 +45,59 @@ public class CategoryE2ETest {
     }
 
     @Test
-    public void testWorks() {
+    public void asCatalogAdminIShouldBeAbleToCreateANewCategoryWithValidValues() throws Exception {
         Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var expectedName = "Filme";
+        final var expectedDescription = "A mais assitida";
+        final var expectedIsActive = true;
+
+        final var actualId = givenAcategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualCategory = retriveACategory(actualId.getValue());
+
+        Assertions.assertEquals(expectedName, actualCategory.name());
+        Assertions.assertEquals(expectedDescription, actualCategory.description());
+        Assertions.assertEquals(expectedIsActive, actualCategory.active());
+        Assertions.assertNotNull(actualCategory.createdAt());
+        Assertions.assertNotNull(actualCategory.updatedAt());
+        Assertions.assertNull(actualCategory.deletedAt());
+
+    }
+
+    private CategoryResponse retriveACategory(final String anId) throws Exception {
+        final var aRequest = get("/categories/" + anId);
+        final var json = this.mvc.perform(aRequest)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse().getContentAsString();
+        return Json.readValue(json, CategoryResponse.class);
+    }
+
+    private CategoryId givenAcategory(final String name, final String description, boolean isActive) throws Exception {
+        final var request = new CreateCategoryRequest(name, description, isActive);
+
+        final var aRequest = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Json.writeValueAsString(request));
+
+        final var actualId = this.mvc.perform(aRequest)
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location")
+                .replace("/categories/", "");
+
+        // <-- Só uma outra forma de fazer a mesm coisa -->
+//        final var actualJson = this.mvc.perform(aRequest)
+//                .andExpect(status().isCreated())
+//                .andReturn()
+//                .getResponse().getContentAsString();
+//        final var response = Json.readValue(actualJson, Map.class);
+//        return CategoryId.from((String) response.get("id"));
+//
+
+        return CategoryId.from(actualId);
     }
 }
