8f387c79a1fb4f18ca1d92cdc6b3b1e4225cc9dc
Nesrux <joaomarcosdevs@gmail.com>
2024-05-21 15:56:13 -0300

    feat: adiciona GenreGateway

---
+++ domain/src/main/java/com/nesrux/admin/catalogo/domain/genre/GenreGateway.java
@@ -0,0 +1,19 @@
+package com.nesrux.admin.catalogo.domain.genre;
+
+import java.util.Optional;
+
+import com.nesrux.admin.catalogo.domain.pagination.Pagination;
+import com.nesrux.admin.catalogo.domain.pagination.SearchQuery;
+
+public interface GenreGateway {
+    Genre create(Genre genre);
+
+    void deleteById(GenreID anId);
+
+    Optional<Genre> findById(GenreID anId);
+
+    Genre update(Genre genre);
+
+    Pagination<Genre> findAll(SearchQuery query);
+
+}
