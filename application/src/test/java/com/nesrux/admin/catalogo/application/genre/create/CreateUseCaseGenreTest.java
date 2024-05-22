d330c628dbcf497244f7c1fce544ecfcf7659dea
Nesrux <joaomarcosdevs@gmail.com>
2024-05-22 14:17:33 -0300

    test: adiciona teste de tdd ao CreateGenreUseCase

---
+++ application/src/test/java/com/nesrux/admin/catalogo/application/genre/create/CreateUseCaseGenreTest.java
@@ -0,0 +1,67 @@
+package com.nesrux.admin.catalogo.application.genre.create;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.nesrux.admin.catalogo.domain.category.CategoryGateway;
+import com.nesrux.admin.catalogo.domain.category.CategoryId;
+import com.nesrux.admin.catalogo.domain.genre.GenreGateway;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateUseCaseGenreTest {
+    @InjectMocks
+    private DefaultCreateGenreUseCase useCase;
+    @Mock
+    private GenreGateway genreGateway;
+    @Mock
+    private CategoryGateway CategoryGateway;
+
+    @Test
+    private void givenAValidCommand_whenCallsCreateGenre_shouldReturnGenreId() {
+        // given
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryId>of();
+
+        final var aCommand = CreateGenreCommand.with(expectedName, expectedIsActive,
+                asString(expectedCategories));
+
+        when(genreGateway.create(any())).thenAnswer(returnsFirstArg());
+
+        // when
+        final var actualOutput = usecase.execute();
+
+        // then
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        verify(genreGateway, times(1))
+                .create(argThat(aGenre -> Objects.equals(expectedName, aGenre.getName())
+                        && Objects.equals(expectedIsActive, aGenre.isActive())
+                        && Objects.equals(expectedCategories, aGenre.getCategories())
+                        && Objects.nonNull(aGenre.getCreatedAt())
+                        && Objects.nonNull(aGenre.getUpdatedAt())
+                        && Objects.isNull(aGenre.getDeletedAt())));
+    }
+
+    private List<String> asString(final List<CategoryId> categories) {
+        return categories.stream()
+                .map(CategoryId::getValue)
+                .toList();
+    }
+}
