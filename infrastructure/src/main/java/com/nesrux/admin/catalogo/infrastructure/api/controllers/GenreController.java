d299ccd14825832da3f5b98fe755ecaa1458ea9b
Nesrux <joaomarcosdevs@gmail.com>
2024-06-02 17:39:57 -0300

    feat: implementa endpoint de List Genre

--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/GenreController.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/GenreController.java
@@ -4,9 +4,11 @@ import com.nesrux.admin.catalogo.application.genre.create.CreateGenreCommand;
 import com.nesrux.admin.catalogo.application.genre.create.CreateGenreUseCase;
 import com.nesrux.admin.catalogo.application.genre.delete.DeleteGenreUseCase;
 import com.nesrux.admin.catalogo.application.genre.retrive.get.GetGenreByIdUseCase;
+import com.nesrux.admin.catalogo.application.genre.retrive.list.ListGenreUseCase;
 import com.nesrux.admin.catalogo.application.genre.update.UpdateGenreCommand;
 import com.nesrux.admin.catalogo.application.genre.update.UpdateGenreUseCase;
 import com.nesrux.admin.catalogo.domain.pagination.Pagination;
+import com.nesrux.admin.catalogo.domain.pagination.SearchQuery;
 import com.nesrux.admin.catalogo.infrastructure.api.GenreAPI;
 import com.nesrux.admin.catalogo.infrastructure.genre.models.CreateGenreRequest;
 import com.nesrux.admin.catalogo.infrastructure.genre.models.GenreListResponse;
@@ -25,16 +27,19 @@ public class GenreController implements GenreAPI {
     private final GetGenreByIdUseCase getGenreByIdUseCase;
     private final UpdateGenreUseCase updateGenreUseCase;
     private final DeleteGenreUseCase deleteGenreUseCase;
+    private final ListGenreUseCase listGenreUseCase;
 
     public GenreController(
             final CreateGenreUseCase createGenreUseCase,
             final GetGenreByIdUseCase getGenreByIdUseCase,
             final UpdateGenreUseCase updateGenreUseCase,
-            final DeleteGenreUseCase deleteGenreUseCase) {
+            final DeleteGenreUseCase deleteGenreUseCase,
+            final ListGenreUseCase listGenreUseCase) {
         this.createGenreUseCase = createGenreUseCase;
         this.getGenreByIdUseCase = getGenreByIdUseCase;
         this.updateGenreUseCase = updateGenreUseCase;
         this.deleteGenreUseCase = deleteGenreUseCase;
+        this.listGenreUseCase = listGenreUseCase;
     }
 
     @Override
@@ -51,8 +56,8 @@ public class GenreController implements GenreAPI {
 
     @Override
     public Pagination<GenreListResponse> list(String search, int page, int perPage, String sort, String dir) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'list'");
+        return this.listGenreUseCase.execute(new SearchQuery(page, perPage, search, sort, dir))
+                .map(GenreApiPresenter::present);
     }
 
     @Override
