517b51becb97b2dd8aa0e4b4ae4493cfdc325e96
Nesrux <joaomarcosdevs@gmail.com>
2024-06-03 14:42:42 -0300

    feat: adiciona gateway de castmember

---
+++ domain/src/main/java/com/nesrux/admin/catalogo/domain/castmember/CastMemberGateway.java
@@ -0,0 +1,18 @@
+package com.nesrux.admin.catalogo.domain.castmember;
+
+import com.nesrux.admin.catalogo.domain.pagination.Pagination;
+import com.nesrux.admin.catalogo.domain.pagination.SearchQuery;
+
+import java.util.Optional;
+
+public interface CastMemberGateway {
+    CastMember create(CastMember aCastMember);
+
+    void deleteById(CastMemberID anId);
+
+    Optional<CastMember> findById(CastMemberID anId);
+
+    CastMember update(CastMember aCastMember);
+
+    Pagination<CastMember> findAll(SearchQuery aQuery);
+}
