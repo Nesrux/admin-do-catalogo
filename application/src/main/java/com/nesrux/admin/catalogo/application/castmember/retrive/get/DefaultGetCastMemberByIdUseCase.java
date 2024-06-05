e4e77a01aeedc76fe5987c21e9b3441f5db4173c
Nesrux <joaomarcosdevs@gmail.com>
2024-06-05 12:07:41 -0300

    feat: implementa GetByID use case de Cast member

---
+++ application/src/main/java/com/nesrux/admin/catalogo/application/castmember/retrive/get/DefaultGetCastMemberByIdUseCase.java
@@ -0,0 +1,24 @@
+package com.nesrux.admin.catalogo.application.castmember.retrive.get;
+
+import com.nesrux.admin.catalogo.domain.castmember.CastMember;
+import com.nesrux.admin.catalogo.domain.castmember.CastMemberGateway;
+import com.nesrux.admin.catalogo.domain.castmember.CastMemberID;
+import com.nesrux.admin.catalogo.domain.exceptions.NotFoundException;
+
+import java.util.Objects;
+
+public final class DefaultGetCastMemberByIdUseCase extends GetCastMemberUseCase {
+    private final CastMemberGateway gateway;
+
+    public DefaultGetCastMemberByIdUseCase(final CastMemberGateway gateway) {
+        this.gateway = Objects.requireNonNull(gateway);
+    }
+
+    @Override
+    public CastMemberOutput execute(final String anId) {
+        final var aMemberID = CastMemberID.from(anId);
+        return this.gateway.findById(aMemberID)
+                .map(CastMemberOutput::from)
+                .orElseThrow(() -> NotFoundException.with(CastMember.class, aMemberID));
+    }
+}
