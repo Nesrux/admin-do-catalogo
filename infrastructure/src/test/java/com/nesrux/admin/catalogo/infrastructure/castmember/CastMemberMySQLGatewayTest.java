63c18e57bfdc798d55d27c66ca58e6ac76cb4661
Nesrux <joaomarcosdevs@gmail.com>
2024-06-06 17:14:50 -0300

    test: adiciona teste de TDD de CastMemberGateway create

--- infrastructure/src/test/java/com/nesrux/admin/catalogo/infrastructure/castmember/CastMemberMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/infrastructure/castmember/CastMemberMySQLGatewayTest.java
@@ -1,10 +1,15 @@
 package com.nesrux.admin.catalogo.infrastructure.castmember;
 
 import com.nesrux.admin.catalogo.MySQLGatewayTest;
+import com.nesrux.admin.catalogo.domain.castmember.CastMember;
 import com.nesrux.admin.catalogo.domain.castmember.CastMemberGateway;
 import com.nesrux.admin.catalogo.infrastructure.castmember.persistence.CastMemberRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.nesrux.admin.catalogo.Fixture.CastMember.type;
+import static com.nesrux.admin.catalogo.Fixture.name;
 
 @MySQLGatewayTest
 public class CastMemberMySQLGatewayTest {
@@ -14,8 +19,42 @@ public class CastMemberMySQLGatewayTest {
     @Autowired
     private CastMemberRepository castMemberRepository;
 
-    private void testDependencies() {
+    @Test
+    public void testDependencies() {
         Assertions.assertNotNull(castMemberGateway);
         Assertions.assertNotNull(castMemberRepository);
+    }
+
+    @Test
+    public void givenAValidCastMember_whenCallsCreate_ShouldPersistIT() {
+        //given
+        final var expectedName = name();
+        final var expectedType = type();
+
+        final var aMember = CastMember
+                .newMember(expectedName, expectedType);
+
+        final var expectedId = aMember.getId();
+        //when
+        Assertions.assertEquals(0, castMemberRepository.count());
+
+        final var actualMember = castMemberGateway.create(CastMember.with(aMember));
+
+        Assertions.assertEquals(1, castMemberRepository.count());
+
+        Assertions.assertEquals(expectedId, actualMember.getId());
+        Assertions.assertEquals(expectedName, actualMember.getName());
+        Assertions.assertEquals(expectedType, actualMember.getType());
+        Assertions.assertEquals(aMember.getCreatedAt(), actualMember.getCreatedAt());
+        Assertions.assertEquals(aMember.getUpdatedAt(), actualMember.getUpdatedAt());
+
+        final var persistedMember = castMemberRepository.findById(expectedId.getValue()).get();
+
+        Assertions.assertEquals(expectedId.getValue(), persistedMember.getId());
+        Assertions.assertEquals(expectedName, persistedMember.getName());
+        Assertions.assertEquals(expectedType, persistedMember.getType());
+        Assertions.assertEquals(aMember.getCreatedAt(), persistedMember.getCreatedAt());
+        Assertions.assertEquals(aMember.getUpdatedAt(), persistedMember.getUpdatedAt());
+
     }
 }
