782d248a31624df8cbfb8eef8a5944ff885e7462
Nesrux <joaomarcosdevs@gmail.com>
2024-06-06 16:56:55 -0300

    feat: adiciona Implementação de CastMemberGateway

---
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/castmember/CastMemberMySQLGateway.java
@@ -0,0 +1,47 @@
+package com.nesrux.admin.catalogo.infrastructure.castmember;
+
+import com.nesrux.admin.catalogo.domain.castmember.CastMember;
+import com.nesrux.admin.catalogo.domain.castmember.CastMemberGateway;
+import com.nesrux.admin.catalogo.domain.castmember.CastMemberID;
+import com.nesrux.admin.catalogo.domain.pagination.Pagination;
+import com.nesrux.admin.catalogo.domain.pagination.SearchQuery;
+import com.nesrux.admin.catalogo.infrastructure.castmember.persistence.CastMemberRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+public class CastMemberMySQLGateway implements CastMemberGateway {
+    private final CastMemberRepository repository;
+
+    public CastMemberMySQLGateway(final CastMemberRepository repository) {
+        this.repository = Objects.requireNonNull(repository);
+    }
+
+
+    @Override
+    public CastMember create(final CastMember aCastMember) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(final CastMemberID anId) {
+
+    }
+
+    @Override
+    public Optional<CastMember> findById(final CastMemberID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CastMember update(final CastMember aCastMember) {
+        return null;
+    }
+
+    @Override
+    public Pagination<CastMember> findAll(final SearchQuery aQuery) {
+        return null;
+    }
+}
