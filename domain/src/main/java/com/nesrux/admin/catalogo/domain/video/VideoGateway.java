60447a749f72cd7af2aa061a373edf4846808471
Nesrux <joaomarcosdevs@gmail.com>
2024-06-17 18:39:48 -0300

    feat: adiciona gateway de Videos

---
+++ domain/src/main/java/com/nesrux/admin/catalogo/domain/video/VideoGateway.java
@@ -0,0 +1,18 @@
+package com.nesrux.admin.catalogo.domain.video;
+
+import com.nesrux.admin.catalogo.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface VideoGateway {
+    Video create(Video aVideo);
+
+    void deleteById(VideoID anId);
+
+    Optional<Video> findById(VideoID anId);
+
+    Video update(Video aVideo);
+
+    Pagination<Video> findAll(VideoSearchQuery aQuery);
+
+}
