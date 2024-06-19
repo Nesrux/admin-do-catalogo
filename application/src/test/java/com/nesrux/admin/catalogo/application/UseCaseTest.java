3b752bd59ad9eda15255b75d637ad9ad6e8a6898
Nesrux <joaomarcosdevs@gmail.com>
2024-06-19 18:36:28 -0300

    feat: adiciona metodo util em usecasTest

--- application/src/test/java/com/nesrux/admin/catalogo/application/UseCaseTest.java
+++ application/src/test/java/com/nesrux/admin/catalogo/application/UseCaseTest.java
@@ -1,7 +1,11 @@
 package com.nesrux.admin.catalogo.application;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import com.nesrux.admin.catalogo.domain.Identifier;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -18,4 +22,15 @@ public abstract class UseCaseTest implements BeforeEachCallback {
 
     protected abstract List<Object> getMocks();
 
+    protected Set<String> asString(final Set<? extends Identifier> ids) {
+        return ids.stream()
+                .map(Identifier::getValue)
+                .collect(Collectors.toSet());
+    }
+
+    protected List<String> asString(final List<? extends Identifier> ids) {
+        return ids.stream()
+                .map(Identifier::getValue)
+                .toList();
+    }
 }
