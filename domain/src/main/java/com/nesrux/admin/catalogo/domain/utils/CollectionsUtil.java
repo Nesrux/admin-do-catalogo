94f5d7763ca837c9532e9200790c63273f07c8b0
Nesrux <joaomarcosdevs@gmail.com>
2024-06-21 17:19:14 -0300

    feat: adiciona classe util de collections

---
+++ domain/src/main/java/com/nesrux/admin/catalogo/domain/utils/CollectionsUtil.java
@@ -0,0 +1,16 @@
+package com.nesrux.admin.catalogo.domain.utils;
+
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public final class CollectionsUtil {
+    private CollectionsUtil() {
+    }
+
+    public static <IN, OUT> Set<OUT> asString(final Set<IN> ids, Function<IN, OUT> mapper) {
+        return ids.stream()
+                .map(mapper)
+                .collect(Collectors.toSet());
+    }
+}
