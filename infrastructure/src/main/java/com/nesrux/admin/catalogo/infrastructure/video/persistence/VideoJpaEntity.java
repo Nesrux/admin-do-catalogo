6d3d6642da0147e304bbd35ae05fa62b49b42680
Nesrux <joaomarcosdevs@gmail.com>
2024-06-24 11:50:00 -0300

    feat: faz relacionamentos com categorys, AudioVideoMedia e imageMedia

--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/video/persistence/VideoJpaEntity.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/video/persistence/VideoJpaEntity.java
@@ -1,5 +1,6 @@
 package com.nesrux.admin.catalogo.infrastructure.video.persistence;
 
+import com.nesrux.admin.catalogo.domain.category.CategoryID;
 import com.nesrux.admin.catalogo.domain.video.Rating;
 import com.nesrux.admin.catalogo.domain.video.Video;
 import com.nesrux.admin.catalogo.domain.video.VideoID;
@@ -7,7 +8,11 @@ import com.nesrux.admin.catalogo.domain.video.VideoID;
 import javax.persistence.*;
 import java.time.Instant;
 import java.time.Year;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Entity(name = "Video")
 @Table(name = "videos")
@@ -43,10 +48,35 @@ public class VideoJpaEntity {
     @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME(6)")
     private Instant updatedAt;
 
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JoinColumn(name = "video_id")
+    private AudioVideoMediaJpaEntity video;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JoinColumn(name = "trailer_id")
+    private AudioVideoMediaJpaEntity trailer;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JoinColumn(name = "banner_id")
+    private ImageMediaJpaEntity banner;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JoinColumn(name = "thumbnail_id")
+    private ImageMediaJpaEntity thumbnail;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    @JoinColumn(name = "thumbnail_half_id")
+    private ImageMediaJpaEntity thumbnailHalf;
+
+    @OneToMany(mappedBy = "video", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<VideoCategoryJpaEntity> categories;
+
+
     public VideoJpaEntity() {
+        this.categories = new HashSet<>(3);
     }
 
-    public VideoJpaEntity(
+    private VideoJpaEntity(
             final UUID id,
             final String title,
             final String description,
@@ -56,7 +86,12 @@ public class VideoJpaEntity {
             final Rating rating,
             final double duration,
             final Instant createdAt,
-            final Instant updatedAt
+            final Instant updatedAt,
+            final AudioVideoMediaJpaEntity video,
+            final AudioVideoMediaJpaEntity trailer,
+            final ImageMediaJpaEntity banner,
+            final ImageMediaJpaEntity thumbnail,
+            final ImageMediaJpaEntity thumbnailHalf
     ) {
         this.id = id;
         this.title = title;
@@ -68,10 +103,16 @@ public class VideoJpaEntity {
         this.duration = duration;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.video = video;
+        this.trailer = trailer;
+        this.banner = banner;
+        this.thumbnail = thumbnail;
+        this.thumbnailHalf = thumbnailHalf;
+        this.categories = new HashSet<>(3);
     }
 
     public static VideoJpaEntity from(final Video aVideo) {
-        return new VideoJpaEntity(
+        final var entity = new VideoJpaEntity(
                 UUID.fromString(aVideo.getId().getValue()),
                 aVideo.getTitle(),
                 aVideo.getDescription(),
@@ -81,8 +122,17 @@ public class VideoJpaEntity {
                 aVideo.getRating(),
                 aVideo.getDuration(),
                 aVideo.getCreatedAt(),
-                aVideo.getUpdatedAt()
+                aVideo.getUpdatedAt(),
+                aVideo.getVideo().map(AudioVideoMediaJpaEntity::from).orElse(null),
+                aVideo.getTrailer().map(AudioVideoMediaJpaEntity::from).orElse(null),
+                aVideo.getBanner().map(ImageMediaJpaEntity::from).orElse(null),
+                aVideo.getThumbnail().map(ImageMediaJpaEntity::from).orElse(null),
+                aVideo.getThumbnailHalf().map(ImageMediaJpaEntity::from).orElse(null)
         );
+
+        aVideo.getCategories().forEach(entity::addCategory);
+
+        return entity;
     }
 
     public Video toAggregate() {
@@ -92,102 +142,170 @@ public class VideoJpaEntity {
                 getDescription(),
                 Year.of(getYerarLaunched()),
                 getDuration(),
-                getOpened(),
-                getPublished(),
+                isOpened(),
+                isPublished(),
                 getRating(),
                 getCreatedAt(),
                 getUpdatedAt(),
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
+                Optional.of(getBanner().toDomain()).orElse(null),
+                Optional.of(getThumbnail().toDomain()).orElse(null),
+                Optional.of(getThumbnailHalf().toDomain()).orElse(null),
+                Optional.of(getTrailer().toDomain()).orElse(null),
+                Optional.of(getVideo().toDomain()).orElse(null),
+                getCategories().stream()
+                        .map(it -> CategoryID.from(it.getId().getCategoryId()))
+                        .collect(Collectors.toSet()),
                 null,
                 null
-
-
         );
 
+    }
+
+    public void addCategory(final CategoryID anId) {
+        this.categories.add(VideoCategoryJpaEntity.from(this, anId));
     }
 
     public UUID getId() {
         return id;
     }
 
-    public void setId(UUID id) {
+    public VideoJpaEntity setId(UUID id) {
         this.id = id;
+        return this;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
+    public VideoJpaEntity setTitle(String title) {
         this.title = title;
+        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    public VideoJpaEntity setDescription(String description) {
         this.description = description;
+        return this;
     }
 
     public int getYerarLaunched() {
         return yerarLaunched;
     }
 
-    public void setYerarLaunched(int yerarLaunched) {
+    public VideoJpaEntity setYerarLaunched(int yerarLaunched) {
         this.yerarLaunched = yerarLaunched;
+        return this;
     }
 
-    public boolean getOpened() {
+    public boolean isOpened() {
         return opened;
     }
 
-    public void setOpened(boolean opened) {
+    public VideoJpaEntity setOpened(boolean opened) {
         this.opened = opened;
+        return this;
     }
 
-    public boolean getPublished() {
+    public boolean isPublished() {
         return published;
     }
 
-    public void setPublished(boolean published) {
+    public VideoJpaEntity setPublished(boolean published) {
         this.published = published;
+        return this;
     }
 
     public Rating getRating() {
         return rating;
     }
 
-    public void setRating(Rating rating) {
+    public VideoJpaEntity setRating(Rating rating) {
         this.rating = rating;
+        return this;
     }
 
     public double getDuration() {
         return duration;
     }
 
-    public void setDuration(double duration) {
+    public VideoJpaEntity setDuration(double duration) {
         this.duration = duration;
+        return this;
     }
 
     public Instant getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(Instant createdAt) {
+    public VideoJpaEntity setCreatedAt(Instant createdAt) {
         this.createdAt = createdAt;
+        return this;
     }
 
     public Instant getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(Instant updatedAt) {
+    public VideoJpaEntity setUpdatedAt(Instant updatedAt) {
         this.updatedAt = updatedAt;
+        return this;
+    }
+
+    public AudioVideoMediaJpaEntity getVideo() {
+        return video;
+    }
+
+    public VideoJpaEntity setVideo(AudioVideoMediaJpaEntity video) {
+        this.video = video;
+        return this;
+    }
+
+    public AudioVideoMediaJpaEntity getTrailer() {
+        return trailer;
+    }
+
+    public VideoJpaEntity setTrailer(AudioVideoMediaJpaEntity trailer) {
+        this.trailer = trailer;
+        return this;
+    }
+
+    public ImageMediaJpaEntity getBanner() {
+        return banner;
+    }
+
+    public VideoJpaEntity setBanner(ImageMediaJpaEntity banner) {
+        this.banner = banner;
+        return this;
+    }
+
+    public ImageMediaJpaEntity getThumbnail() {
+        return thumbnail;
+    }
+
+    public VideoJpaEntity setThumbnail(ImageMediaJpaEntity thumbnail) {
+        this.thumbnail = thumbnail;
+        return this;
+    }
+
+    public ImageMediaJpaEntity getThumbnailHalf() {
+        return thumbnailHalf;
+    }
+
+    public VideoJpaEntity setThumbnailHalf(ImageMediaJpaEntity thumbnailHalf) {
+        this.thumbnailHalf = thumbnailHalf;
+        return this;
+    }
+
+    public Set<VideoCategoryJpaEntity> getCategories() {
+        return categories;
+    }
+
+    public VideoJpaEntity setCategories(Set<VideoCategoryJpaEntity> categories) {
+        this.categories = categories;
+        return this;
     }
 }
