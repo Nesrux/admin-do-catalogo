f273001d8584f5f6908fe410fa6fb70d1f7926ec
Nesrux <joaomarcosdevs@gmail.com>
2024-06-25 12:27:23 -0300

    feat: adiciona repository de Video

---
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/video/persistence/VideoRepository.java
@@ -0,0 +1,8 @@
+package com.nesrux.admin.catalogo.infrastructure.video.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface VideoRepository extends JpaRepository<VideoJpaEntity, UUID> {
+}
