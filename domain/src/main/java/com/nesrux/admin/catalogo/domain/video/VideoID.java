fe91ffafb656e3c65294a4cab1919e77d799b464
Nesrux <joaomarcosdevs@gmail.com>
2024-06-25 13:05:52 -0300

    refactor: refatorando os identfiers de Genre e Video

--- domain/src/main/java/com/nesrux/admin/catalogo/domain/video/VideoID.java
+++ domain/src/main/java/com/nesrux/admin/catalogo/domain/video/VideoID.java
@@ -1,7 +1,7 @@
 package com.nesrux.admin.catalogo.domain.video;
 
 import com.nesrux.admin.catalogo.domain.Identifier;
-import com.nesrux.admin.catalogo.domain.genre.GenreID;
+import com.nesrux.admin.catalogo.domain.utils.IdUtils;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -19,16 +19,13 @@ public class VideoID extends Identifier {
     }
 
     public static VideoID unique() {
-        return VideoID.from(UUID.randomUUID());
+        return VideoID.from(IdUtils.uuid());
     }
 
     public static VideoID from(final String AnId) {
         return new VideoID(AnId);
     }
 
-    public static VideoID from(final UUID anId) {
-        return new VideoID(anId.toString().toLowerCase());
-    }
 
     @Override
     public int hashCode() {
