0b4a8a57f424ed52ce45ebdaa37df88c3a9fbc79
Nesrux <joaomarcosdevs@gmail.com>
2024-07-01 15:41:17 -0300

    feat: cria service de Storage

---
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/services/StorageService.java
@@ -0,0 +1,19 @@
+package com.nesrux.admin.catalogo.infrastructure.services;
+
+import com.nesrux.admin.catalogo.domain.video.Resource;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface StorageService {
+
+    void deleteAll(Collection<String> names);
+
+    Optional<Resource> get(String name);
+
+    List<String> list(String prefix);
+
+    void store(String name, Resource resource);
+
+}
