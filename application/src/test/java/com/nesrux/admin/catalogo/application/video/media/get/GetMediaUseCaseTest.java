f86f73676eea74585a47d57562f6c485a4f4a571
João Marcos <joaomarcosx100@gmail.com>
2024-07-09 15:49:52 -0300

    test: adiciona teste de Tdd de Get MediaResource Use Case

---
+++ application/src/test/java/com/nesrux/admin/catalogo/application/video/media/get/GetMediaUseCaseTest.java
@@ -0,0 +1,73 @@
+package com.nesrux.admin.catalogo.application.video.media.get;
+
+import com.nesrux.admin.catalogo.application.UseCaseTest;
+import com.nesrux.admin.catalogo.domain.Fixture;
+import com.nesrux.admin.catalogo.domain.exceptions.NotFoundException;
+import com.nesrux.admin.catalogo.domain.video.MediaResourceGateway;
+import com.nesrux.admin.catalogo.domain.video.VideoID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+public class GetMediaUseCaseTest extends UseCaseTest {
+    @InjectMocks
+    private DefaultGetMediaUseCase useCase;
+    @Mock
+    private MediaResourceGateway mediaResourceGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(mediaResourceGateway);
+    }
+
+    @Test
+    public void givenVideoIdAndType_whenIsValidCommand_thenReturnResource() {
+        //given
+        final var expectedId = VideoID.unique();
+        final var expectedType = Fixture.Videos.ramdomVideoMediaType();
+        final var expectedResource = Fixture.Videos.resource(expectedType);
+
+        when(mediaResourceGateway.getResource(expectedId, expectedType))
+                .thenReturn(Optional.of(expectedResource));
+
+        final var aCommand = GetMediaCommand.with(expectedId.getValue(), expectedType.name());
+
+        //when
+        final var actualResult = this.useCase.execute(aCommand);
+
+        //then
+        Assertions.assertNotNull(actualResult);
+        Assertions.assertEquals(expectedResource, actualResult);
+
+    }
+
+    @Test
+    public void givenVideoIdAndType_whenIsNotFound_thenReturNotFoundException() {
+        //given
+        final var expectedId = VideoID.unique();
+        final var expectedType = Fixture.Videos.ramdomVideoMediaType();
+        final var expectedResource = Fixture.Videos.resource(expectedType);
+
+        when(mediaResourceGateway.getResource(expectedId, expectedType))
+                .thenReturn(Optional.empty());
+
+        final var aCommand = GetMediaCommand.with(expectedId.getValue(), expectedType.name());
+
+        //when
+        final var actualResult = Assertions.assertThrows(NotFoundException.class,
+                () -> this.useCase.execute(aCommand));
+
+        //then
+
+
+    }
+
+
+}
