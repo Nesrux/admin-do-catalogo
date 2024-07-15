d4ab533520b28c9c5c0ffb09691e24b83dbc7127
Nesrux <joaomarcosdevs@gmail.com>
2024-07-15 16:02:02 -0300

    feat: adiciona VideoMediaCreated

---
+++ domain/src/main/java/com/nesrux/admin/catalogo/domain/video/VideoMediaCreated.java
@@ -0,0 +1,22 @@
+package com.nesrux.admin.catalogo.domain.video;
+
+import com.nesrux.admin.catalogo.domain.event.DomainEvent;
+import com.nesrux.admin.catalogo.domain.utils.InstantUtils;
+
+import java.time.Instant;
+
+public record VideoMediaCreated(
+        String resourceId,
+        String filePath,
+        Instant occurredOn
+) implements DomainEvent {
+
+    public VideoMediaCreated(final String resourceId, final String filePath) {
+        this(resourceId, filePath, InstantUtils.now());
+    }
+
+    @Override
+    public Instant occurrentOn() {
+        return occurredOn;
+    }
+}
