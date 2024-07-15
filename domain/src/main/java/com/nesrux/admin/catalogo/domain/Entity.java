b40eb57e268f436ec686ae5ab68abb4948208760
Nesrux <joaomarcosdevs@gmail.com>
2024-07-15 15:42:10 -0300

    refactor: refatora as classes de eventos

--- domain/src/main/java/com/nesrux/admin/catalogo/domain/Entity.java
+++ domain/src/main/java/com/nesrux/admin/catalogo/domain/Entity.java
@@ -21,7 +21,6 @@ public abstract class Entity<ID extends Identifier> {
     }
 
 
-
     public ID getId() {
         return id;
     }
@@ -30,7 +29,7 @@ public abstract class Entity<ID extends Identifier> {
         return Collections.unmodifiableList(domainEvents);
     }
 
-    public void publishDomainEvent(final DomainEventPublisher publisher) {
+    public void publishDomainEvent(final DomainEventPublisher<DomainEvent> publisher) {
         if (publisher == null) return;
         getDomainEvents()
                 .forEach(publisher::publishEvent);
