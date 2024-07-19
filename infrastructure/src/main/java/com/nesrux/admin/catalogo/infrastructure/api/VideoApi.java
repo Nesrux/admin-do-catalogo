c4d30518cfad755e3d01c2c7c49f71f3acdabc4c
Nesrux <joaomarcosdevs@gmail.com>
2024-07-19 13:45:19 -0300

    feat: implementa endpoint de criação de videos com todos os parametros

--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/VideoApi.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/VideoApi.java
@@ -1,9 +1,45 @@
 package com.nesrux.admin.catalogo.infrastructure.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Set;
 
 @RequestMapping(value = "videos")
 @Tag(name = "video")
 public interface VideoApi {
+    @PostMapping(
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a new Video with medias")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "video created successfully"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal sercer error was thrown")
+    })
+    ResponseEntity<?> createFull(
+            @RequestParam(name = "title", required = false) String title,
+            @RequestParam(name = "description", required = false) String description,
+            @RequestParam(name = "year_launched", required = false) Integer yearLaunched,
+            @RequestParam(name = "duration", required = false) Double duration,
+            @RequestParam(name = "rating", required = false) String rating,
+            @RequestParam(name = "opened", required = false) Boolean opened,
+            @RequestParam(name = "published", required = false) Boolean published,
+            @RequestParam(name = "categories_id", required = false) Set<String> categories,
+            @RequestParam(name = "genres_id", required = false) Set<String> genres,
+            @RequestParam(name = "cast_members_id", required = false) Set<String> members,
+            @RequestParam(name = "video_file", required = false) MultipartFile videoFile,
+            @RequestParam(name = "trailer_file", required = false) MultipartFile trailerFile,
+            @RequestParam(name = "banner_file", required = false) MultipartFile bannerFile,
+            @RequestParam(name = "thumb_file", required = false) MultipartFile thumbFile,
+            @RequestParam(name = "thumb_half_file", required = false) MultipartFile thumbHalfFile
+    );
 }
--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/VideoController.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/VideoController.java
@@ -1,8 +1,79 @@
 package com.nesrux.admin.catalogo.infrastructure.api.controllers;
 
+import com.nesrux.admin.catalogo.application.video.create.CreateVideoCommand;
+import com.nesrux.admin.catalogo.application.video.create.CreateVideoUseCase;
+import com.nesrux.admin.catalogo.domain.resource.Resource;
 import com.nesrux.admin.catalogo.infrastructure.api.VideoApi;
+import com.nesrux.admin.catalogo.infrastructure.utils.HashingUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
+import java.util.Objects;
+import java.util.Set;
 
 @RestController
 public class VideoController implements VideoApi {
+    private final CreateVideoUseCase useCase;
+
+    public VideoController(final CreateVideoUseCase useCase) {
+        this.useCase = Objects.requireNonNull(useCase);
+    }
+
+    @Override
+    public ResponseEntity<?> createFull(
+            final String aTitle,
+            final String aDescription,
+            final Integer launchedAt,
+            final Double aDuration,
+            final String aRating,
+            final Boolean wasOpened,
+            final Boolean wasPublished,
+            final Set<String> categories,
+            final Set<String> genres,
+            final Set<String> members,
+            final MultipartFile videoFile,
+            final MultipartFile trailerFile,
+            final MultipartFile bannerFile,
+            final MultipartFile thumbFile,
+            final MultipartFile thumbHalfFile
+    ) {
+        final var aCommd = CreateVideoCommand.with(
+                aTitle,
+                aDescription,
+                launchedAt,
+                aDuration,
+                wasOpened,
+                wasPublished,
+                aRating,
+                categories,
+                genres,
+                members,
+                resourceOf(videoFile),
+                resourceOf(trailerFile),
+                resourceOf(bannerFile),
+                resourceOf(thumbFile),
+                resourceOf(thumbHalfFile));
+
+        final var output = this.useCase.execute(aCommd);
+        return ResponseEntity.created(URI.create("/videos/" + output.id())).body(output);
+    }
+
+    private Resource resourceOf(final MultipartFile file) {
+        if (file == null) return null;
+        try {
+            return Resource.with(
+                    HashingUtils.checkSum(file.getBytes()),
+                    file.getBytes(),
+                    file.getContentType(),
+                    file.getOriginalFilename()
+            );
+
+
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
 }
+
