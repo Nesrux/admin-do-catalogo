f52e1c221e0b33b0226fb31d6590742e1045380c
Nesrux <joaomarcosdevs@gmail.com>
2024-07-23 17:32:13 -0300

    feat: implementa o endpoint de listagem de video

--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/VideoController.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/api/controllers/VideoController.java
@@ -4,13 +4,21 @@ import com.nesrux.admin.catalogo.application.video.create.CreateVideoCommand;
 import com.nesrux.admin.catalogo.application.video.create.CreateVideoUseCase;
 import com.nesrux.admin.catalogo.application.video.delete.DeleteVideoUseCase;
 import com.nesrux.admin.catalogo.application.video.retrive.get.GetVideoByIdUseCase;
+import com.nesrux.admin.catalogo.application.video.retrive.list.ListVideosUseCase;
 import com.nesrux.admin.catalogo.application.video.update.UpdateVideoCommand;
 import com.nesrux.admin.catalogo.application.video.update.UpdateVideoUseCase;
+import com.nesrux.admin.catalogo.domain.castmember.CastMemberID;
+import com.nesrux.admin.catalogo.domain.category.CategoryID;
+import com.nesrux.admin.catalogo.domain.genre.GenreID;
+import com.nesrux.admin.catalogo.domain.pagination.Pagination;
 import com.nesrux.admin.catalogo.domain.resource.Resource;
+import com.nesrux.admin.catalogo.domain.utils.CollectionsUtil;
+import com.nesrux.admin.catalogo.domain.video.VideoSearchQuery;
 import com.nesrux.admin.catalogo.infrastructure.api.VideoApi;
 import com.nesrux.admin.catalogo.infrastructure.utils.HashingUtils;
 import com.nesrux.admin.catalogo.infrastructure.video.models.api.CreateVideoRequest;
 import com.nesrux.admin.catalogo.infrastructure.video.models.api.UpdateVideoRequest;
+import com.nesrux.admin.catalogo.infrastructure.video.models.api.VideoListResponse;
 import com.nesrux.admin.catalogo.infrastructure.video.models.api.VideoResponse;
 import com.nesrux.admin.catalogo.infrastructure.video.presenters.VideoApiPresenter;
 import org.springframework.http.ResponseEntity;
@@ -21,22 +29,28 @@ import java.net.URI;
 import java.util.Objects;
 import java.util.Set;
 
+import static com.nesrux.admin.catalogo.domain.utils.CollectionsUtil.mapTo;
+
 @RestController
 public class VideoController implements VideoApi {
     private final CreateVideoUseCase createVideoUseCase;
     private final GetVideoByIdUseCase getVideoByIdUseCase;
     private final UpdateVideoUseCase updateVideoUseCase;
     private final DeleteVideoUseCase deleteVideoUseCase;
+    private final ListVideosUseCase listVideosUseCase;
 
     public VideoController(
             final CreateVideoUseCase createVideoUseCase,
             final GetVideoByIdUseCase getVideoByIdUseCase,
             final UpdateVideoUseCase updateVideoUseCase,
-            final DeleteVideoUseCase deleteVideoUseCase) {
+            final DeleteVideoUseCase deleteVideoUseCase,
+            final ListVideosUseCase listVideosUseCase) {
+
         this.createVideoUseCase = Objects.requireNonNull(createVideoUseCase);
         this.getVideoByIdUseCase = Objects.requireNonNull(getVideoByIdUseCase);
         this.updateVideoUseCase = Objects.requireNonNull(updateVideoUseCase);
         this.deleteVideoUseCase = Objects.requireNonNull(deleteVideoUseCase);
+        this.listVideosUseCase = Objects.requireNonNull(listVideosUseCase);
     }
 
     @Override
@@ -127,6 +141,26 @@ public class VideoController implements VideoApi {
     @Override
     public void deleteById(final String id) {
         this.deleteVideoUseCase.execute(id);
+    }
+
+    @Override
+    public Pagination<VideoListResponse> list(
+            final String search,
+            final int page,
+            final int perPage,
+            final String sort,
+            final String direction,
+            final Set<String> castMembers,
+            final Set<String> categories,
+            final Set<String> genres) {
+
+        final var membersId = mapTo(castMembers, CastMemberID::from);
+        final var categoriesId = mapTo(categories, CategoryID::from);
+        final var genreIds = mapTo(genres, GenreID::from);
+
+        final var aQuery = new VideoSearchQuery(page, perPage, search, sort, direction, membersId, categoriesId, genreIds);
+        final var aPage = this.listVideosUseCase.execute(aQuery);
+        return VideoApiPresenter.present(aPage);
     }
 
     private Resource resourceOf(final MultipartFile file) {
--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/video/presenters/VideoApiPresenter.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/video/presenters/VideoApiPresenter.java
@@ -1,13 +1,12 @@
 package com.nesrux.admin.catalogo.infrastructure.video.presenters;
 
 import com.nesrux.admin.catalogo.application.video.retrive.get.VideoOutput;
+import com.nesrux.admin.catalogo.application.video.retrive.list.VideoListOutput;
 import com.nesrux.admin.catalogo.application.video.update.UpdateVideoOutput;
+import com.nesrux.admin.catalogo.domain.pagination.Pagination;
 import com.nesrux.admin.catalogo.domain.video.AudioVideoMedia;
 import com.nesrux.admin.catalogo.domain.video.ImageMedia;
-import com.nesrux.admin.catalogo.infrastructure.video.models.api.AudioVideoMediaResponse;
-import com.nesrux.admin.catalogo.infrastructure.video.models.api.ImageMediaResponse;
-import com.nesrux.admin.catalogo.infrastructure.video.models.api.UpdateVideoResponse;
-import com.nesrux.admin.catalogo.infrastructure.video.models.api.VideoResponse;
+import com.nesrux.admin.catalogo.infrastructure.video.models.api.*;
 
 public interface VideoApiPresenter {
     static VideoResponse present(final VideoOutput output) {
@@ -52,5 +51,20 @@ public interface VideoApiPresenter {
 
     static UpdateVideoResponse present(final UpdateVideoOutput output) {
         return new UpdateVideoResponse(output.id());
+    }
+
+    static VideoListResponse present(final VideoListOutput output) {
+        return new VideoListResponse(
+                output.id(),
+                output.title(),
+                output.description(),
+                output.createdAt(),
+                output.updatedAt()
+        );
+
+    }
+
+    static Pagination<VideoListResponse> present(final Pagination<VideoListOutput> aPage) {
+        return aPage.map(VideoApiPresenter::present);
     }
 }
