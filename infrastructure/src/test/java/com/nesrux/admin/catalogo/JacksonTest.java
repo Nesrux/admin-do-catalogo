7e91d4a2bf22b64511022c85bcec593dcae5f2e1
Nesrux <joaomarcosdevs@gmail.com>
2024-07-26 10:49:42 -0300

    chore: adiciona as tags de e2e e integration testes

--- infrastructure/src/test/java/com/nesrux/admin/catalogo/JacksonTest.java
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/JacksonTest.java
@@ -1,6 +1,7 @@
 package com.nesrux.admin.catalogo;
 
 import com.nesrux.admin.catalogo.infrastructure.configuration.ObjectMapperConfig;
+import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -12,6 +13,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @ActiveProfiles("test-integration")
+@Tag("integrationTest")
 @JsonTest(includeFilters = {
                 @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ObjectMapperConfig.class)
 })
