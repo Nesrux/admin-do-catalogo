3df4f0f7e39241531e9235c612bb9f1adc5d512f
Nesrux <joaomarcosdevs@gmail.com>
2024-07-27 15:45:20 -0300

    ci: fix ci

--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/castmember/persistence/CastMemberRepository.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/castmember/persistence/CastMemberRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CastMemberRepository extends JpaRepository<CastMemberJpaEntity, String> {
 
     Page<CastMemberJpaEntity> findAll(Specification<CastMemberJpaEntity> specification, Pageable page);
