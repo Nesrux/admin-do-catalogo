2b9dd252201372f5c061067e4fe9e13aec822506
Nesrux <joaomarcosdevs@gmail.com>
2024-07-30 11:49:47 -0300

    feat: configura o jwt converter

--- infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/configuration/SecurityConfig.java
+++ infrastructure/src/main/java/com/nesrux/admin/catalogo/infrastructure/configuration/SecurityConfig.java
@@ -1,12 +1,25 @@
 package com.nesrux.admin.catalogo.infrastructure.configuration;
 
+import com.nimbusds.jose.shaded.json.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableWebSecurity
@@ -33,9 +46,7 @@ public class SecurityConfig {
                                     .anyRequest().hasRole(ROLE_ADMIN);
                         })
                 .oauth2ResourceServer(oauth -> {
-                    oauth.jwt();
-                    //TODO : Proxima aula
-                    //.jwtAuthenticationConverter()
+                    oauth.jwt().jwtAuthenticationConverter(new KeyCloakJWTConverter());
                 }).sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
@@ -43,5 +54,74 @@ public class SecurityConfig {
                     headers.frameOptions().sameOrigin();
                 })
                 .build();
+    }
+
+    static class KeyCloakJWTConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+        private final KeyCloakAuthoritiesConverter authoritiesConverter;
+
+        KeyCloakJWTConverter() {
+            this.authoritiesConverter = new KeyCloakAuthoritiesConverter();
+        }
+
+        @Override
+        public AbstractAuthenticationToken convert(final Jwt jwt) {
+
+            return new JwtAuthenticationToken(jwt, extractAuthorities(jwt), extractPrincipal(jwt));
+        }
+
+        private String extractPrincipal(final Jwt jwt) {
+            return jwt.getClaimAsString(JwtClaimNames.SUB);
+        }
+
+        private Collection<? extends GrantedAuthority> extractAuthorities(final Jwt jwt) {
+            return this.authoritiesConverter.convert(jwt);
+        }
+    }
+
+    static class KeyCloakAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+        public static final String REALM_ACCESS = "realm_access";
+        public static final String ROLES = "roles";
+        public static final String RESOURCE_ACCESS = "resource_access";
+
+        @Override
+        public Collection<GrantedAuthority> convert(final Jwt jwt) {
+            final var reamRoles = extractRealmRoles(jwt);
+            final var resourceRoles = extractResourceRoles(jwt);
+            return Stream.concat(reamRoles, resourceRoles)
+                    .map(role -> new SimpleGrantedAuthority(role.toUpperCase()))
+                    .collect(Collectors.toSet());
+        }
+
+        private Stream<String> extractResourceRoles(final Jwt jwt) {
+            final Function<Map.Entry<String, Object>, Stream<String>> mapResource =
+                    resource -> {
+                        final var key = resource.getKey();
+                        final var value = (JSONObject) resource.getValue();
+                        final var roles = (Collection<String>) value.get(ROLES);
+                        return roles.stream()
+                                .map(role -> key.concat("_").concat(ROLES));
+                    };
+
+            final Function<Set<Map.Entry<String, Object>>, Collection<String>> mapResources =
+                    resources -> resources.stream()
+                            .flatMap(mapResource)
+                            .toList();
+
+            return Optional.ofNullable(jwt.getClaimAsMap(RESOURCE_ACCESS))
+                    .map(Map::entrySet)
+                    .map(mapResources)
+                    .orElse(Collections.emptyList())
+                    .stream();
+
+        }
+
+        private Stream<String> extractRealmRoles(final Jwt jwt) {
+            return Optional.ofNullable(jwt.getClaimAsMap(REALM_ACCESS))
+                    .map(resource -> (Collection<String>) resource.get(ROLES))
+                    .orElse(Collections.emptyList())
+                    .stream();
+
+        }
     }
 }
