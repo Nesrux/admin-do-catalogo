fbbc56b81e7de573ea057bc464ff2bf47c82d829
Nesrux <joaomarcosdevs@gmail.com>
2024-08-01 17:07:20 -0300

    fix: resolve erro dos testes de integration

--- infrastructure/src/test/java/com/nesrux/admin/catalogo/MySQLGatewayTest.java
+++ infrastructure/src/test/java/com/nesrux/admin/catalogo/MySQLGatewayTest.java
@@ -13,9 +13,11 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @ActiveProfiles("test-integration")
-@ComponentScan(basePackages = "com.nesrux.admin.catalogo", includeFilters = {
-        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".[MySQLGateway]")
-})
+@ComponentScan(basePackages = "com.nesrux.admin.catalogo",
+        useDefaultFilters = false,
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*MySQLGateway")
+        })
 @DataJpaTest
 @ExtendWith(MySQLCleanUpExtension.class)
 @Tag("integrationTest")
